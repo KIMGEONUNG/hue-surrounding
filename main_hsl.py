0fbd5d30b4cd2ae8cc581510246ede3abcae63de
KIMGEONUNG <saywooong@gmail.com>
2020-06-13 16:15:50 +0900

    complete hsl variation

--- main_hsl.py
+++ main_hsl.py
@@ -5,60 +5,26 @@ import sys
 import getopt
 
 args = sys.argv[1:]
-opts, args = getopt.getopt(args, 'r:g:b:p:o:h')
-
-class value_rule(Enum):
-    ZERO = 0
-    PLUS = 1
-    MINUS = 2
+opts, args = getopt.getopt(args, 's:p:o:h')
 
 # Basic parameters
-interval=0.005
+# interval=0.02
+# step=50
+interval=0.05
 step=20
 images = []
 path = "./images/sample1.jpg"
 output = './output.gif'
 
-ruleR = value_rule.PLUS
-ruleG = value_rule.PLUS
-ruleB = value_rule.PLUS
-
 for opt, arg in opts:
-    if opt == "-r":
-        if arg == "0":
-            ruleR = value_rule.ZERO
-        elif arg == "1":
-            ruleR = value_rule.PLUS
-        elif arg == "2":
-            ruleR = value_rule.MINUS
-        else:
-            raise ValueError("")
-    elif opt == "-g":
-        if arg == "0":
-            ruleG = value_rule.ZERO
-        elif arg == "1":
-            ruleG = value_rule.PLUS
-        elif arg == "2":
-            ruleG = value_rule.MINUS
-        else:
-            raise ValueError("")
-    elif opt == "-b":
-        if arg == "0":
-            ruleB = value_rule.ZERO
-        elif arg == "1":
-            ruleB = value_rule.PLUS
-        elif arg == "2":
-            ruleB = value_rule.MINUS
-        else:
-            raise ValueError("")
-    elif opt == "-p":
+    if opt == "-p":
         path = arg
+    elif opt == "-s":
+        step = int(arg) 
+        interval = 1 / step
     elif opt == "-o":
         output = arg
     elif opt == "-h":
-        print("-r : R of RGB value sign. 0 is no change, 1 is plus, 2 is minus. Default value is 1")
-        print("-g : G of RGB value sign. 0 is no change, 1 is plus, 2 is minus. Default value is 1")
-        print("-b : B of RGB value sign. 0 is no change, 1 is plus, 2 is minus. Default value is 1")
         print("-p : Input image file path. Default path is \"%s\"" % path)
         print("-o : output image file path. Default path is \"%s\"" % output)
         sys.exit(1)
@@ -84,16 +50,11 @@ for i in range(0,step):
     
     new_data = []
     for d in data:
-        try:
-            hsl = colorsys.rgb_to_hls(d[0],d[1],d[2])
-        except:
-            pass
-        finally:
-            hsl = [0,0,0]
+        hsl = colorsys.rgb_to_hls(d[0]/255,d[1]/255,d[2]/255)
 
-        new_h = hsl[0] + val
-        new_rgb = colorsys.hls_to_rgb(new_h, d[1], d[2])
-        new_rgb_int = (int(new_rgb[0]), int(new_rgb[1]), int(new_rgb[2]))
+        new_h = (hsl[0] + val)
+        new_rgb = colorsys.hls_to_rgb(new_h, hsl[1], hsl[2])
+        new_rgb_int = (int(new_rgb[0] * 255), int(new_rgb[1] * 255), int(new_rgb[2] * 255))
         new_data.append(new_rgb_int)
 
     im = Image.new('RGB', (width, height))
